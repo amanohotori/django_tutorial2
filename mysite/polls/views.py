d4d55412b9f7322599352a188811e0bb836ca7eb
Hotori Amano <amanohotorii@gmail.com>
2022-06-09 16:00:56 +0900

    定時コミット

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+import re
+from urllib import response
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,3 +8,13 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You' ")
